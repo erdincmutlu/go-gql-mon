562e7407e65cbbc289012aa8976c7e2e54f761f1
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-07-04 19:29:02 +0100

    Database functions implemented

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
 # go-gql-mon
 Go GraphQL MongoDB CRUD Project
+
+Mongo server on docker
+```
+docker run -d -p 27017-27019:27017-27019 --name mongodb mongo:latest
+```
+
+
+#### Get All Jobs
+
+```
+query GetAllJobs{
+  jobs{
+    _id
+    title
+    description
+    company
+    url
+  }
+}
+```
+
+
+#### Create Job
+
+```
+mutation CreateJobListing($input: CreateJobListingInput!){
+  createJobListing(input:$input){
+    _id
+    title
+    description
+    company
+    url
+  }
+}
+
+{
+  "input": {
+    "title": "Software Development Engineer - I",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    "company": "Google",
+    "url": "www.google.com/"
+  }
+}
+```
+
+#### Get Job By Id
+
+```
+query GetJob($id: ID!){
+  job(id:$id){
+    _id
+    title
+    description
+    url
+    company
+  }
+}
+```
+
+
+```
+{
+  "id": "638051d7acc418c13197fdf7"
+}
+```
+
+#### Update Job By Id
+
+```
+mutation UpdateJob($id: ID!,$input: UpdateJobListingInput!) {
+  updateJobListing(id:$id,input:$input){
+    title
+    description
+    _id
+    company
+    url
+  }
+}
+```
+
+
+```
+{
+  "id": "638051d3acc418c13197fdf6",
+  "input": {
+    "title": "Software Development Engineer - III"
+  }
+}
+```
+
+
+
+#### Delete Job By Id
+
+```
+mutation DeleteQuery($id: ID!) {
+  deleteJobListing(id:$id){
+    deleteJobId
+  }
+}
+```
+
+```
+{
+  "id": "638051d3acc418c13197fdf6"
+}
+```
--- database/database.go
+++ database/database.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	"github.com/erdincmutlu/go-gql-mon/graph/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -42,25 +44,107 @@ func Connect() *DB {
 }
 
 func (db *DB) GetJob(id string) *model.JobListing {
+	jobCollection := db.client.Database("graphql-job-board").Collection("jobs")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	_id, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.M{"_id": _id}
 	var jobListing model.JobListing
+	err := jobCollection.FindOne(ctx, filter).Decode(&jobListing)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &jobListing
 }
 
 func (db *DB) GetJobs() []*model.JobListing {
+	jobCollection := db.client.Database("graphql-job-board").Collection("jobs")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
 	var jobListings []*model.JobListing
+	cursor, err := jobCollection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cursor.All(ctx, &jobListings)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return jobListings
 }
 
 func (db *DB) CreateJobListing(jobInfo model.CreateJobListingInput) *model.JobListing {
-	var returnJobListing model.JobListing
+	jobCollection := db.client.Database("graphql-job-board").Collection("jobs")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	inserted, err := jobCollection.InsertOne(ctx, bson.M{
+		"title":       jobInfo.Title,
+		"description": jobInfo.Description,
+		"company":     jobInfo.Company,
+		"url":         jobInfo.URL,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	insertedID := inserted.InsertedID.(primitive.ObjectID).Hex()
+	returnJobListing := model.JobListing{
+		ID:          insertedID,
+		Title:       jobInfo.Title,
+		Description: jobInfo.Description,
+		Company:     jobInfo.Company,
+		URL:         jobInfo.URL,
+	}
 	return &returnJobListing
 }
 
 func (db *DB) UpdateJobListing(jobId string, jobInfo model.UpdateJobListingInput) *model.JobListing {
+	jobCollection := db.client.Database("graphql-job-board").Collection("jobs")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	updateJobInfo := bson.M{}
+	if jobInfo.Title != nil {
+		updateJobInfo["title"] = jobInfo.Title
+	}
+	if jobInfo.Description != nil {
+		updateJobInfo["description"] = jobInfo.Description
+	}
+	if jobInfo.URL != nil {
+		updateJobInfo["url"] = jobInfo.URL
+	}
+
+	_id, _ := primitive.ObjectIDFromHex(jobId)
+	filter := bson.M{"_id": _id}
+	update := bson.M{"$set": updateJobInfo}
+
+	results := jobCollection.FindOneAndUpdate(ctx, filter, update, options.FindOneAndUpdate().SetReturnDocument(options.After))
+
 	var jobListing model.JobListing
+	err := results.Decode(&jobListing)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return &jobListing
 }
 
 func (db *DB) DeleteJobListing(jobId string) *model.DeleteJobResponse {
+	jobCollection := db.client.Database("graphql-job-board").Collection("jobs")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	_id, _ := primitive.ObjectIDFromHex(jobId)
+	filter := bson.M{"_id": _id}
+	_, err := jobCollection.DeleteOne(ctx, filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &model.DeleteJobResponse{DeleteJobID: jobId}
 }
