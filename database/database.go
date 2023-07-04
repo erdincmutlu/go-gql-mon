72f16fc6b10b5c310572585eb3f1189feb558e1c
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-07-04 14:51:02 +0100

    Database connect

--- database/database.go
+++ database/database.go
@@ -1,1 +1,66 @@
 package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/erdincmutlu/go-gql-mon/graph/model"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var connectionString string = "mongodb://localhost:27017/test"
+
+type DB struct {
+	client *mongo.Client
+}
+
+func Connect() *DB {
+	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &DB{
+		client: client,
+	}
+}
+
+func (db *DB) GetJob(id string) *model.JobListing {
+	var jobListing model.JobListing
+	return &jobListing
+}
+
+func (db *DB) GetJobs() []*model.JobListing {
+	var jobListings []*model.JobListing
+	return jobListings
+}
+
+func (db *DB) CreateJobListing(jobInfo model.CreateJobListingInput) *model.JobListing {
+	var returnJobListing model.JobListing
+	return &returnJobListing
+}
+
+func (db *DB) UpdateJobListing(jobId string, jobInfo model.UpdateJobListingInput) *model.JobListing {
+	var jobListing model.JobListing
+	return &jobListing
+}
+
+func (db *DB) DeleteJobListing(jobId string) *model.DeleteJobResponse {
+	return &model.DeleteJobResponse{DeleteJobID: jobId}
+}
